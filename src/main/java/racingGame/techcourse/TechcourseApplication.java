6487ef85261658c82bd8c88086611facfa3258a0
PUPAO <tkdldjstm0127@gmail.com>
2025-11-22 17:39:18 +0900

    Initial commit

---
+++ src/main/java/racingGame/techcourse/TechcourseApplication.java
@@ -0,0 +1,13 @@
+package racingGame.techcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechcourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/racingGame/techcourse/TechcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package racingGame.techcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
