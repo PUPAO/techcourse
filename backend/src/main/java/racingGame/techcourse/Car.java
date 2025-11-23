b4a4d15113550990409b096adf4452253b6a6479
PUPAO <tkdldjstm0127@gmail.com>
2025-11-23 15:28:46 +0900

    fix(car): 웹 게임용 객체 변경

--- backend/src/main/java/racingGame/techcourse/Car.java
+++ backend/src/main/java/racingGame/techcourse/Car.java
@@ -1,37 +1,38 @@
 package racingGame.techcourse;
 
+import lombok.Data;
+
+@Data
 public class Car {
 
-    private final String racer;
+    private String name;
     private int distance;
+    private int loseCount = 0;
 
-    private static final int limitNameLength = 5;
-
-    private Car(String racer) {
-        this.racer = racer;
-        this.distance = 0;
-    }
-
-    public static Car of(String racerName) {
-
-        if (racerName.length() > limitNameLength) {
-            throw new racingException("Your name is too long. Be careful not to exceed" + limitNameLength + " characters.");
-        } else if (racerName.isEmpty()) {
-            throw new racingException("There is no name. Please create a name.");
-        }
-
-        return new Car(racerName);
-    }
-
-    public int getDistance() {
-        return distance;
-    }
-
-    public String getRacer() {
-        return racer;
-    }
+    public Car() {}
 
     public void goForward() {
         distance++;
+    }
+
+    // 이겼을 때: 앞으로 가고 라운드 증가
+    public void win() {
+        distance++;
+    }
+
+    // 졌을 때: 패배 카운트 올리고 라운드 증가
+    public void lose() {
+        loseCount++;
+        distance++;
+    }
+
+    // 비겼을 때: 라운드만 증가
+    public void draw() {
+        distance++;
+    }
+
+    // 3번 이상 지면 게임 종료
+    public boolean isGameOver() {
+        return loseCount >= 3;
     }
 }
