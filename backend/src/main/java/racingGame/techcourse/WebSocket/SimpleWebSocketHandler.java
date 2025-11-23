a90446855e90cf9eb079ba4afd09c235a88b033c
PUPAO <tkdldjstm0127@gmail.com>
2025-11-23 15:37:18 +0900

    feat(racingGame): RacingGame.java를 소켓 통신용 코드로 변환

--- backend/src/main/java/racingGame/techcourse/WebSocket/SimpleWebSocketHandler.java
+++ backend/src/main/java/racingGame/techcourse/WebSocket/SimpleWebSocketHandler.java
@@ -1,39 +1,105 @@
 package racingGame.techcourse.WebSocket;
 
-import java.util.Set;
+import java.io.IOException;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import racingGame.techcourse.Car;
+import racingGame.techcourse.MoveRule;
 
 @Component
 public class SimpleWebSocketHandler extends TextWebSocketHandler {
 
-    private final Set<WebSocketSession> sessions = ConcurrentHashMap.newKeySet();
+    private final Map<String, Car> players = new ConcurrentHashMap<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
-        sessions.add(session);
+        players.put(session.getId(), new Car());
         System.out.println("Connected : " + session.getId());
     }
 
+    @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        String payload = message.getPayload();
-        System.out.println("received message : " + payload );
+        JSONObject json = new JSONObject(message.getPayload());
+        String type = json.getString("type");
 
-        for(WebSocketSession s : sessions){
-            if(s.isOpen()){
-                s.sendMessage(new TextMessage(payload));
-            }
+        if ("JOIN".equals(type)) {
+            handleJoin(session, json);
+        } else if ("PLAY".equals(type)) {
+            handlePlay(session, json);
+        } else if ("END".equals(type)) {
+            handleEnd(session);
         }
     }
 
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
-        sessions.remove(session);
-        System.out.println("disconnected!");
+    private void handleJoin(WebSocketSession session, JSONObject json) throws IOException {
+        Car state = players.get(session.getId());
+        state.setName(json.getString("name"));
+
+        JSONObject res = new JSONObject();
+        res.put("type", "START");
+        res.put("message", "게임 시작!");
+        session.sendMessage(new TextMessage(res.toString()));
     }
 
+    private void handlePlay(WebSocketSession session, JSONObject json) throws IOException {
+        Car car = players.get(session.getId());
 
+        String playerChoice = json.getString("choice");
+        String computerChoice = MoveRule.randomHand();
+
+        String result = MoveRule.calculate(playerChoice, computerChoice);
+
+        if (result.equals("WIN")) {
+            car.win();
+        } else if (result.equals("LOSE")) {
+            car.lose();
+        } else {
+            car.draw();
+        }
+
+        JSONObject res = new JSONObject();
+        res.put("type", "RESULT");
+        res.put("playerChoice", playerChoice);
+        res.put("computerChoice", computerChoice);
+        res.put("result", result);
+        res.put("distance", car.getDistance());
+
+        session.sendMessage(new TextMessage(res.toString()));
+
+        if (car.isGameOver()) {
+            JSONObject finish = new JSONObject();
+            finish.put("type", "FINISH");
+            finish.put("score", car.getDistance());
+            finish.put("name", car.getName());
+            session.sendMessage(new TextMessage(finish.toString()));
+        }
+    }
+
+    private void handleEnd(WebSocketSession session) throws IOException {
+        Car state = players.get(session.getId());
+
+        // DB 저장 로직 수행
+        int score = state.getDistance();
+
+        JSONObject res = new JSONObject();
+        res.put("type", "FINISH");
+        res.put("score", score);
+
+        session.sendMessage(new TextMessage(res.toString()));
+
+        players.remove(session.getId());
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        players.remove(session.getId());
+        System.out.println("Disconnect: " + session.getId() + " / " + status);
+
+    }
 }
